91056f84cf11847310eb156a01f5509db1a60309
DanielRivas18 <jose.rivas@uptc.edu.co>
2024-06-10 17:51:32 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.set('PORT',process.env.PORT || 3000 )
+app.set('PORT', process.env.PORT || 3000)
 
 app.use('/api/products', require('./routes/routes-products'))
 
 const swaggerUI = require('swagger-ui-express')
 const swaggerSpec = require('./swagger')
-app.use('/docs',swaggerUI.serve,swaggerUI.setup(swaggerSpec))
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
-app.listen(app.get('PORT'),()=>console.log(`Server Ready al port ${app.get('PORT')}`))
+app.listen(app.get('PORT'), () => console.log(`Server Ready al port ${app.get('PORT')}`))
